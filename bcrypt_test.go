91eda3de49458027f0112d6f273c8e6b0cab2fcc
hunter007 <wentao79@gmail.com>
2023-05-25 16:54:20 +0800

    test
    Signed-off-by: hunter007 <wentao79@gmail.com>

---
+++ bcrypt_test.go
@@ -0,0 +1,25 @@
+package passwordvalidator
+
+import "testing"
+
+func TestBcrypt(t *testing.T) {
+	opt := &HasherOption{
+		Algorithm:  bcryptAlgo,
+		Salt:       "salt",
+		Iterations: 1,
+	}
+	password := "1qasw23"
+	hasher, err := NewHasher(opt)
+	if err != nil {
+		t.Errorf("failed to new %s hasher: %s", opt.Algorithm, err)
+	}
+
+	encoded, err := hasher.Encode(password)
+	if err != nil {
+		t.Errorf("failed to encode password with %s: %s", opt.Algorithm, err)
+	}
+	t.Logf("encoded password: %s", encoded)
+	if !hasher.Verify(password, encoded) {
+		t.Errorf("wrong algorithm: %s", opt.Algorithm)
+	}
+}
